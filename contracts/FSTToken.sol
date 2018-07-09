5ffd3b11e772c8f272b5608cb273be240ee6247b
rajapattanayak <pattanayak.raja@gmail.com>
2018-07-09 21:17:27 +0530

    Delegated Transfer

--- contracts/FSTToken.sol
+++ contracts/FSTToken.sol
@@ -6,11 +6,17 @@ contract FSTToken {
 
     uint256 totalSupply_;
     mapping(address => uint256) balances;
+    mapping(address => mapping (address => uint256)) internal allowed;
 
     event Transfer(
         address indexed _from,
         address indexed _to,
         uint256 _value 
+    );
+    event Approval(
+        address indexed _owner, 
+        address indexed _spender, 
+        uint256 _value
     );
 
     constructor (uint256 _initialSupply) public {
@@ -34,6 +40,34 @@ contract FSTToken {
         
         emit Transfer(msg.sender, _to, _value);
         
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
+        allowed[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+    
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return allowed[_owner][_spender];
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_to != msg.sender);
+        require(balances[_from] >= _value);
+        require(allowed[_from][msg.sender] >= _value);
+        
+        balances[_from] -= _value;
+        balances[_to] += _value;
+
+        allowed[_from][msg.sender] -= _value;
+        
+        emit Transfer(_from, _to, _value);
         return true;
     }
 }
--- test/FSTToken.js
+++ test/FSTToken.js
@@ -30,11 +30,11 @@ contract("FSTToken", accounts => {
       });
     });
 
-    describe('Transfer', () => {
+    describe('Direct Transfer', () => {
       it('should not transfer token if sender does not have required balance', async () => {
         try {
           // Does not create a transaction
-          await instance.transfer.call(accounts[1],'2222222222222222');
+          await instance.transfer.call(accounts[1],2222222222222222);
           assert(false);
         } catch(err) {
           assert(err.message.indexOf('revert') >= 0, 'err msg must contain the word revert');
@@ -43,7 +43,7 @@ contract("FSTToken", accounts => {
 
       it('should transfer token successfully', async () => {
         // Trigger a transaction 
-        const receipt = await instance.transfer(accounts[1], 100000, { from: accounts[0]});
+        const receipt = await instance.transfer(accounts[1], 100000, { from: admin });
         const { event, args } = receipt.logs[0];
         const { _from, _to, _value } = args;
         
@@ -59,5 +59,78 @@ contract("FSTToken", accounts => {
         const senderBalance = await instance.balanceOf.call(accounts[0]);
         assert.equal(senderBalance.toNumber(), 900000, 'token should be successfully removed from adminaccount');
       })
+    })
+
+    describe('Delegated Transfer', () => {
+      it('should approves tokens for delegated transfer', async () => {
+        const response = await instance.approve.call(accounts[1], 100);
+        assert.equal(response, true);
+
+        const receipt = await instance.approve(accounts[1], 100, { from : admin });
+        const { event, args } = receipt.logs[0];
+        const { _owner, _spender, _value } = args;
+        
+        assert.equal(receipt.logs.length, 1, 'triggers a event');
+        assert.equal(event, 'Approval', 'should be the "Approval" event');
+        assert.equal(_owner, admin, 'logs the account from which tokens are transferred');
+        assert.equal(_spender, accounts[1], 'logs the account to which tokens are transferred');
+        assert.equal(_value, 100, 'logs the token transfer amount');
+
+        const allowance =  await instance.allowance(admin, accounts[1]);
+        assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegated transfer');
+      });
+      
+      it('should not perform delegated transfer', async () => {
+        let spendingAccount = accounts[1];
+
+        let fromAccount = accounts[2];
+        let toAccount = accounts[3];
+
+        await instance.transfer(fromAccount, 50000, { from: admin });
+        await instance.approve(spendingAccount, 100, { from : fromAccount });
+
+        try {
+          await instance.transferFrom(fromAccount, toAccount, 90000, { from : spendingAccount });
+          assert(false);
+        } catch(err) {
+          assert(err.message.indexOf('revert') >= 0, 'can not transfer token more than the balance');
+        }
+
+        try {
+          await instance.transferFrom(fromAccount, toAccount, 500, { from : spendingAccount });
+          assert(false);
+        } catch(err) {
+          assert(err.message.indexOf('revert') >= 0, 'can not transfer token more than approved');
+        }
+      })
+
+      it('should perform delegated transfer', async () => {
+        let fromAccount = accounts[4];
+
+        let toAccount = accounts[5];
+        let spendingAccount = accounts[6];
+
+        await instance.transfer(fromAccount, 50000, { from: admin });
+        await instance.approve(spendingAccount, 100, { from : fromAccount });
+        const receipt = await instance.transferFrom(fromAccount, toAccount, 40, { from : spendingAccount });
+        const { event, args } = receipt.logs[0];
+        const { _from, _to, _value } = args;
+        
+        assert.equal(receipt.logs.length, 1, 'triggers a event');
+        assert.equal(event, 'Transfer', 'should be the "Transfer" event');
+        assert.equal(_from, fromAccount, 'logs the account from which tokens are transferred');
+        assert.equal(_to, toAccount, 'logs the account to which tokens are transferred');
+        assert.equal(_value, 40, 'logs the token transfer amount');
+        
+        const fromBalance = await instance.balanceOf.call(fromAccount);
+        assert.equal(fromBalance.toNumber(), 49960, 'token should be successfully removed from sender'); 
+
+        const toBalance = await instance.balanceOf.call(toAccount);
+        assert.equal(toBalance.toNumber(), 40, 'token should be added successfully to recipient'); 
+
+        const toAllowance = await instance.allowance(fromAccount, spendingAccount);
+        assert.equal(toAllowance.toNumber(), 60, 'token allowance should be deducted'); 
+      })
+
     })
 });
